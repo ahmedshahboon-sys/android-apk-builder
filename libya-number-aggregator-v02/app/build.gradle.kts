b5ac1ad2dc456a653d02500736b88eac16b7410d
ahmedshahboon-sys <ahmed.shahboon@gmail.com>
2026-08-27 05:17:39 +0200

    Add number aggregator app build file

---
+++ libya-number-aggregator-v02/app/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.shahboun.numberlookup"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.shahboun.numberlookup"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 2
+        versionName = "0.2.0-dev"
+    }
+
+    buildFeatures { viewBinding = true }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+}
