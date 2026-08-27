f221777e810169bd26f510acfafdd6933e44323f
ahmedshahboon-sys <ahmed.shahboon@gmail.com>
2026-08-27 05:56:05 +0200

    Bump number aggregator to v0.4

--- libya-number-aggregator-v02/app/build.gradle.kts
+++ libya-number-aggregator-v02/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.shahboun.numberlookup"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.3.0-dev"
+        versionCode = 4
+        versionName = "0.4.0-dev"
     }
 
     buildFeatures { viewBinding = true }
