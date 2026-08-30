fa26da63199a692eb2ec97361b2354a220b1a1e0
ahmedshahboon-sys <ahmed.shahboon@gmail.com>
2026-08-30 20:13:31 +0200

    Initialize Shahboun runtime instrumentation

--- ShahbounMultiProject/app/src/main/java/com/shahboun/multi/MultiApplication.kt
+++ ShahbounMultiProject/app/src/main/java/com/shahboun/multi/MultiApplication.kt
@@ -3,12 +3,13 @@ package com.shahboun.multi
 import android.app.Application
 
 class MultiApplication : Application() {
-    lateinit var engine: CloneEngine
+    lateinit var engine: ShahbounCloneEngine
         private set
 
     override fun onCreate() {
         super.onCreate()
         engine = ShahbounCloneEngine()
         engine.initialize(this).getOrThrow()
+        RuntimeInstrumentationInstaller.install().getOrThrow()
     }
 }
