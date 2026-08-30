062aa57970973fae7cab081d20a274cf0fe0b83b
ahmedshahboon-sys <ahmed.shahboon@gmail.com>
2026-08-30 19:10:06 +0200

    Fix E5785 v1.5.0 build metadata

--- router-e5785/app/build.gradle.kts
+++ router-e5785/app/build.gradle.kts
@@ -6,8 +6,8 @@ android {
         applicationId = "com.shahboun.e5785"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.3.0"
+        versionCode = 6
+        versionName = "1.5.0"
     }
     buildTypes { release { isMinifyEnabled = false } }
 }
