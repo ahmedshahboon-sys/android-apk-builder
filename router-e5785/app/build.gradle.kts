4eb7688a2b0ba76e827a9d6485197179b9edb840
ahmedshahboon-sys <ahmed.shahboon@gmail.com>
2026-08-30 19:24:35 +0200

    Release Shahboun Router E5785 v1.6.0

--- router-e5785/app/build.gradle.kts
+++ router-e5785/app/build.gradle.kts
@@ -6,8 +6,8 @@ android {
         applicationId = "com.shahboun.e5785"
         minSdk = 24
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.5.0"
+        versionCode = 7
+        versionName = "1.6.0"
     }
     buildTypes { release { isMinifyEnabled = false } }
 }
