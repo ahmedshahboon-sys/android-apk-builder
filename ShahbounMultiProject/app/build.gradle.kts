710bc9cc75e5d7457a47ed71fa6bd4210ee256ee
ahmedshahboon-sys <ahmed.shahboon@gmail.com>
2026-08-31 01:57:28 +0200

    Bump Shahboun Multi to 0.4.6

--- ShahbounMultiProject/app/build.gradle.kts
+++ ShahbounMultiProject/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.shahboun.multi"
         minSdk = 29
         targetSdk = 36
-        versionCode = 10
-        versionName = "0.4.5"
+        versionCode = 11
+        versionName = "0.4.6"
         manifestPlaceholders["debugActivityEnabled"] = "true"
     }
 
