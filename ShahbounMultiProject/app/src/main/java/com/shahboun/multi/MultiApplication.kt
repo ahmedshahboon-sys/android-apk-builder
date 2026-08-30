db65d9eae57ec75524436cf86f1dae51442cc503
ahmedshahboon-sys <ahmed.shahboon@gmail.com>
2026-08-30 20:20:27 +0200

    Make Shahboun runtime bridge fail safely

--- ShahbounMultiProject/app/src/main/java/com/shahboun/multi/MultiApplication.kt
+++ ShahbounMultiProject/app/src/main/java/com/shahboun/multi/MultiApplication.kt
@@ -6,10 +6,24 @@ class MultiApplication : Application() {
     lateinit var engine: ShahbounCloneEngine
         private set
 
+    @Volatile
+    var runtimeBridgeError: Throwable? = null
+        private set
+
     override fun onCreate() {
         super.onCreate()
         engine = ShahbounCloneEngine()
         engine.initialize(this).getOrThrow()
-        RuntimeInstrumentationInstaller.install().getOrThrow()
+        runtimeBridgeError = RuntimeInstrumentationInstaller.install().exceptionOrNull()
+    }
+
+    fun requireRuntimeBridge() {
+        val retry = RuntimeInstrumentationInstaller.install()
+        if (retry.isSuccess) {
+            runtimeBridgeError = null
+            return
+        }
+        runtimeBridgeError = retry.exceptionOrNull()
+        throw IllegalStateException("تعذر تهيئة طبقة تشغيل Shahboun على هذا الجهاز", runtimeBridgeError)
     }
 }
