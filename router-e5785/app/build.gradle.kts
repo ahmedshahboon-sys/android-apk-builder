53179d811e1bf3b854ccfe991e30d11e996e9341
ahmedshahboon-sys <ahmed.shahboon@gmail.com>
2026-08-30 20:00:57 +0200

    build(router): bump E5785 to 1.6.1

--- router-e5785/app/build.gradle.kts
+++ router-e5785/app/build.gradle.kts
@@ -6,8 +6,8 @@ android {
         applicationId = "com.shahboun.e5785"
         minSdk = 24
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.6.0"
+        versionCode = 8
+        versionName = "1.6.1"
     }
     buildTypes { release { isMinifyEnabled = false } }
 }
