1c33bfc86dfa716ec6902d35867cb4c7592683e6
ahmedshahboon-sys <ahmed.shahboon@gmail.com>
2026-08-31 00:30:28 +0200

    Bump Shahboun Multi to 0.4.2 after inflater classloader fix

--- ShahbounMultiProject/app/build.gradle.kts
+++ ShahbounMultiProject/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.shahboun.multi"
         minSdk = 29
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.4.1"
+        versionCode = 7
+        versionName = "0.4.2"
         manifestPlaceholders["debugActivityEnabled"] = "true"
     }
 
