80e69d6d2d112ac3d296fc8fa2a18744bdc560a1
ahmedshahboon-sys <ahmed.shahboon@gmail.com>
2026-08-31 05:45:13 +0200

    Map guest services to matching clone processes

--- ShahbounMultiProject/app/src/main/java/com/shahboun/multi/RuntimeStubActivity.kt
+++ ShahbounMultiProject/app/src/main/java/com/shahboun/multi/RuntimeStubActivity.kt
@@ -1,6 +1,7 @@
 package com.shahboun.multi
 
 import android.app.Activity
+import android.app.Service
 import android.os.Bundle
 import android.widget.TextView
 
@@ -31,8 +32,16 @@ object RuntimeProcessPool {
         RuntimeStubActivity3::class.java,
         RuntimeStubActivity4::class.java
     )
+    private val serviceStubs: Array<Class<out Service>> = arrayOf(
+        RuntimeStubService0::class.java,
+        RuntimeStubService1::class.java,
+        RuntimeStubService2::class.java,
+        RuntimeStubService3::class.java,
+        RuntimeStubService4::class.java
+    )
 
     fun processIndex(packageName: String, slot: Int): Int = Math.floorMod(31 * packageName.hashCode() + slot, activityStubs.size)
     fun activityStub(packageName: String, slot: Int): Class<out Activity> = activityStubs[processIndex(packageName, slot)]
+    fun serviceStub(packageName: String, slot: Int): Class<out Service> = serviceStubs[processIndex(packageName, slot)]
     fun isActivityStubName(name: String?): Boolean = name == RuntimeStubActivity::class.java.name || activityStubs.any { it.name == name }
 }
