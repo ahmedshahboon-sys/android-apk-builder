d5f9585bf88f9e40a6ee0aa643054b47893a4a4d
ahmedshahboon-sys <ahmed.shahboon@gmail.com>
2026-08-31 03:29:46 +0200

    Release Shahboun Multi 0.5.1 resource and theme compatibility build

--- ShahbounMultiProject/app/build.gradle.kts
+++ ShahbounMultiProject/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.shahboun.multi"
         minSdk = 29
         targetSdk = 36
-        versionCode = 15
-        versionName = "0.5.0"
+        versionCode = 16
+        versionName = "0.5.1"
         manifestPlaceholders["debugActivityEnabled"] = "true"
     }
 
