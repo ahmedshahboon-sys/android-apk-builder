d2bf83f2b787571801060a906589e45f06076201
ahmedshahboon-sys <ahmed.shahboon@gmail.com>
2026-08-31 15:40:53 +0200

    Fix ShortcutInfo builder compatibility alias

--- ShahbounMultiProject/app/src/main/java/com/shahboun/multi/AndroidAliases.kt
+++ ShahbounMultiProject/app/src/main/java/com/shahboun/multi/AndroidAliases.kt
@@ -1,4 +1,10 @@
 package com.shahboun.multi
 
+import android.content.Context
+
 typealias ShortcutManager = android.content.pm.ShortcutManager
-typealias ShortcutInfo = android.content.pm.ShortcutInfo
+
+object ShortcutInfo {
+    fun Builder(context: Context, id: String): android.content.pm.ShortcutInfo.Builder =
+        android.content.pm.ShortcutInfo.Builder(context, id)
+}
