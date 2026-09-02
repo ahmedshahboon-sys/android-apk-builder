fc3627d8b614d0897dffffdaa48826b99775413d
ahmedshahboon-sys <ahmed.shahboon@gmail.com>
2026-09-02 14:08:24 +0200

    Bump Runtime 3 process fix release to 0.7.1 build 29

--- ShahbounMultiProject/app/build.gradle.kts
+++ ShahbounMultiProject/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.shahboun.multi"
         minSdk = 29
         targetSdk = 36
-        versionCode = 28
-        versionName = "0.7.0-runtime3"
+        versionCode = 29
+        versionName = "0.7.1-runtime3"
         manifestPlaceholders["debugActivityEnabled"] = "true"
     }
 
