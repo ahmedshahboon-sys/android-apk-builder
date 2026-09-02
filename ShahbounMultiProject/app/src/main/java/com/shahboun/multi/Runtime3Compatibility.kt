ef0400932d1f09d7b7f060045ad742ba105ce491
ahmedshahboon-sys <ahmed.shahboon@gmail.com>
2026-09-02 03:45:18 +0200

    Remove final Runtime 2 engine compatibility name

--- ShahbounMultiProject/app/src/main/java/com/shahboun/multi/Runtime3Compatibility.kt
+++
@@ -1,4 +0,0 @@
-package com.shahboun.multi
-
-/** Temporary source compatibility only. There is no Runtime 2 implementation behind this name. */
-typealias ShahbounCloneEngine = ShahbounRuntime3Engine
