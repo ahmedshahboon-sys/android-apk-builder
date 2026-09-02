1ab97d4c30e21019220ebc955237de9ae3df355c
ahmedshahboon-sys <ahmed.shahboon@gmail.com>
2026-09-02 03:38:40 +0200

    Add compile compatibility alias for Runtime 3 migration

---
+++ ShahbounMultiProject/app/src/main/java/com/shahboun/multi/Runtime3Compatibility.kt
@@ -0,0 +1,4 @@
+package com.shahboun.multi
+
+/** Temporary source compatibility only. There is no Runtime 2 implementation behind this name. */
+typealias ShahbounCloneEngine = ShahbounRuntime3Engine
