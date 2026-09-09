3e0425ea9dd7d7ae696d63b6e7b88d8997b6f23e
ahmedshahboon-sys <ahmed.shahboon@gmail.com>
2026-09-09 06:26:39 +0200

    Bump Shahboun Multi runtime build to 0.7.8

--- ShahbounMultiProject/app/build.gradle.kts
+++ ShahbounMultiProject/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.shahboun.multi"
         minSdk = 29
         targetSdk = 36
-        versionCode = 35
-        versionName = "0.7.7-runtime3"
+        versionCode = 36
+        versionName = "0.7.8-runtime3"
         manifestPlaceholders["debugActivityEnabled"] = "true"
     }
 
