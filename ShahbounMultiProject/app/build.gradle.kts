281e10655d59599671873086677880e8652226b9
ahmedshahboon-sys <ahmed.shahboon@gmail.com>
2026-09-09 06:01:48 +0200

    Bump Shahboun Multi for segmented diagnostics

--- ShahbounMultiProject/app/build.gradle.kts
+++ ShahbounMultiProject/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.shahboun.multi"
         minSdk = 29
         targetSdk = 36
-        versionCode = 34
-        versionName = "0.7.6-runtime3"
+        versionCode = 35
+        versionName = "0.7.7-runtime3"
         manifestPlaceholders["debugActivityEnabled"] = "true"
     }
 
