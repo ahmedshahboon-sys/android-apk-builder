75af141623ad483ab6c6f79f71e97a94a0dfee67
ahmedshahboon-sys <ahmed.shahboon@gmail.com>
2026-09-09 07:12:35 +0200

    Bump Shahboun Multi Runtime 3 identity fix build

--- ShahbounMultiProject/app/build.gradle.kts
+++ ShahbounMultiProject/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.shahboun.multi"
         minSdk = 29
         targetSdk = 36
-        versionCode = 37
-        versionName = "0.7.9-runtime3"
+        versionCode = 38
+        versionName = "0.8.0-runtime3"
         manifestPlaceholders["debugActivityEnabled"] = "true"
     }
 
